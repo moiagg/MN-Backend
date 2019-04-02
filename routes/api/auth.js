121f966dbdcbcceab4860f7331e37bc1bac19b66
moikapy <warrenckhan@gmail.com>
2019-04-02 01:51:39 -0400

    cleaned route to allow for login and registration

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -35,67 +35,61 @@ router.post("/auth", (req, res) => {
   }).then(user => {
     //check for User
     if (!user) {
-      //   errors.email = "User not Found";
-      //   return res.status(404).json(errors);
-      if (user) {
-        errors.email = "Email already exists";
-        return res.status(400).json({
-          errors
-        });
-      } else {
-        const avatar = gravatar.url(req.body.email, {
-          s: "200", // size
-          r: "pg", // rating
-          d: "mm" // Default
-        });
 
-        const newUser = new Auth({
-          email: req.body.email,
-          avatar,
-          password: req.body.password
-        });
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", // size
+        r: "pg", // rating
+        d: "mm" // Default
+      });
 
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser
-              .save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err));
-          });
-        });
-      }
+      const newUser = new Auth({
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        })
+      })
+    } else {
+      //check Password
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          //User Matched
+          const payload = {
+            // Creates JWT Payload
+            id: user.id,
+            avatar: user.avatar
+          };
+          // Sign Token
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            {
+              expiresIn: 3600
+            },
+            (err, token) => {
+              res.json({
+                success: true,
+                token: `Bearer ${token}`
+              });
+            }
+          );
+        } else {
+          errors.password = "Password incorrect";
+          return res.status(400).json(errors);
+        }
+      })
     }
 
-    //check Password
-    bcrypt.compare(password, user.password).then(isMatch => {
-      if (isMatch) {
-        //User Matched
-        const payload = {
-          // Creates JWT Payload
-          id: user.id,
-          avatar: user.avatar
-        };
-        // Sign Token
-        jwt.sign(
-          payload,
-          keys.secretOrKey,
-          {
-            expiresIn: 3600
-          },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: `Bearer ${token}`
-            });
-          }
-        );
-      } else {
-        errors.password = "Password incorrect";
-        return res.status(400).json(errors);
-      }
-    });
+
   });
 });
 
